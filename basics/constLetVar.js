4ee3dd72779e98b201bc1009efcaed2cf86808d6
Mahesh M. Giri <ermaheshgiri@gmail.com>
2025-06-30 10:09:36 +0000

    constvar

---
+++ basics/constLetVar.js
@@ -0,0 +1,12 @@
+const myname = "mahesh";
+let age = 25;
+var condition = "good";
+
+console.log(myname);
+// we cannot redeclare const that's why we are getting error
+
+const mysisname = "mahi";
+age = 28;
+console.log(myname);
+console.log(age);
+

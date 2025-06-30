9e23a3a06a728b67cb2deabc7c94058f27b7e848
Mahesh M. Giri <ermaheshgiri@gmail.com>
2025-06-30 10:35:49 +0000

    conversiona

---
+++ basics/conversion.js
@@ -0,0 +1,22 @@
+let age = 33 ;
+console.log(typeof age)
+
+let yourage = "33"
+console.log(typeof yourage);
+
+let yourage1 = Number(yourage);
+console.log( typeof yourage1);
+
+let hello ;
+console.log(typeof hello);
+
+let age2 = '33abc';
+console.log(typeof age2);
+
+let age3 = Number(age2);
+console.log(typeof age3);
+console.log(age3);
+
+
+
+
